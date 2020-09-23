874f08a7bebc0cfb665c8c95882369b3da87e50f
Aaron Badger <aaron.badger@purestorage.com>
2020-09-23 17:28:47 +0000

    renamed files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,9 +4,9 @@ variable "private_key_path" {
 }
 variable "fa_url" {
   description = "Cloud Block Store IP Address"
-  default = "10.0.0.25"
+  default = "10.0.1.237"
 }
 variable "pure_api_token" {
   description = "Cloud Block Store API Token"
-  default = "8e8c0984-8761-c3d2-2dcb-f3313b0db294"
+  default = "265be729-0ccc-f69b-abf8-f94122eeaf42"
 }
